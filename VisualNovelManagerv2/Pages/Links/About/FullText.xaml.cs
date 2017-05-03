d46cf7137263a62c01051fc680cdbb34943a2b94
Micah686 <legendofzeldafan3@gmail.com>
2017-05-03 13:44:34 -0700

    created a fulltext for licenses, and added another license page

---
+++ VisualNovelManagerv2/Pages/Links/About/FullText.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace VisualNovelManagerv2.Pages.Links.About
+{
+    /// <summary>
+    /// Interaction logic for FullText.xaml
+    /// </summary>
+    public partial class FullText : UserControl
+    {
+        public FullText()
+        {
+            InitializeComponent();
+        }
+    }
+}
