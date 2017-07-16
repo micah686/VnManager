303dbf34507aec27b4423e6471ca4acec96bc3bc
Micah686 <legendofzeldafan3@gmail.com>
2017-07-16 12:57:38 -0700

    Added basic XML user settings

---
+++ VisualNovelManagerv2/CustomClasses/ModifyUserSettings.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using VisualNovelManagerv2.Design.Settings;
+
+namespace VisualNovelManagerv2.CustomClasses
+{
+    public class ModifyUserSettings
+    {
+        public static void SaveUserSettings(UserSettings userSettings)
+        {
+            try
+            {
+                XmlSerializer mySerializer = new XmlSerializer(typeof(UserSettings));
+                StreamWriter myWriter = new StreamWriter(Globals.DirectoryPath + @"/Data/config/config.xml");
+                mySerializer.Serialize(myWriter, userSettings);
+                myWriter.Close();
+                myWriter.Dispose();
+            }
+            catch (Exception ex)
+            {
+                DebugLogging.WriteDebugLog(ex);
+                throw;
+            }
+        }
+
+        public static UserSettings LoadUserSettings()
+        {
+            try
+            {
+                XmlSerializer mySerializer = new XmlSerializer(typeof(UserSettings));
+                FileStream fileStream = new FileStream(Globals.DirectoryPath + @"/Data/config/config.xml", FileMode.Open, FileAccess.Read, FileShare.Read);
+                return (UserSettings) mySerializer.Deserialize(fileStream);
+            }
+            catch (Exception ex)
+            {
+                DebugLogging.WriteDebugLog(ex);
+                throw;
+            }
+        }
+    }
+}
---
+++ VisualNovelManagerv2/Design/Settings/UserSettings.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VisualNovelManagerv2.Design.Settings
+{
+    public class UserSettings
+    {
+        public bool NsfwEnabled { get; set; }
+        public int MaxSpoilerLevel { get; set; }
+        public VnSettings VnSettings { get; set; }
+    }
+
+    public class VnSettings
+    {
+        public int Id { get; set; }
+        public bool NsfwEnabled { get; set; }
+        public int Spoiler { get; set; }
+    }
+}
