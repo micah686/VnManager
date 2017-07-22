c3f618d4608a39150060b706cada424fb0631a39
Micah686 <legendofzeldafan3@gmail.com>
2017-07-22 11:57:24 -0700

    Made a cleaner version of UserSettings

---
+++ VisualNovelManagerv2/Converters/YesNoBoolConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace VisualNovelManagerv2.Converters
+{
+    [ValueConversion(typeof(bool), typeof(bool))]
+    public class YesNoBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var boolValue = value is bool && (bool)value;
+
+            return boolValue ? "Yes" : "No";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value != null && value.ToString() == "Yes";
+        }
+    }
+}
--- VisualNovelManagerv2/ViewModel/Settings/UserSettingsViewModel.cs
+++ VisualNovelManagerv2/ViewModel/Settings/UserSettingsViewModel.cs
@@ -20,8 +20,8 @@ namespace VisualNovelManagerv2.ViewModel.Settings
         #region Properties
 
         #region SelectedSpoilerLevel
-        private SpoilerLevel _selectedSpoilerLevel = SpoilerLevel.None;
-        public SpoilerLevel SelectedSpoilerLevel
+        private string _selectedSpoilerLevel;
+        public string SelectedSpoilerLevel
         {
             get { return _selectedSpoilerLevel; }
             set
@@ -29,17 +29,17 @@ namespace VisualNovelManagerv2.ViewModel.Settings
                 _selectedSpoilerLevel = value;
                 switch (value)
                 {
-                    case SpoilerLevel.None:
-                        ChosenSpoilerLevel = 0;
+                    case "None":
+                        SpoilerLevel = 0;
                         break;
-                    case SpoilerLevel.Minor:
-                        ChosenSpoilerLevel = 1;
+                    case "Minor":
+                        SpoilerLevel = 1;
                         break;
-                    case SpoilerLevel.Major:
-                        ChosenSpoilerLevel = 2;
+                    case "Major":
+                        SpoilerLevel = 2;
                         break;
                     default:
-                        ChosenSpoilerLevel = 0;
+                        SpoilerLevel = 0;
                         break;
                 }
                 RaisePropertyChanged(nameof(SelectedSpoilerLevel));
@@ -47,132 +47,71 @@ namespace VisualNovelManagerv2.ViewModel.Settings
         }
         #endregion
 
-        #region SpoilerLevelList
-        private List<KeyValuePair<string, SpoilerLevel>> _spoilerLevelList;
-        public List<KeyValuePair<string, SpoilerLevel>> SpoilerLevelList
+        #region SpoilerLevelCollection
+        private ObservableCollection<string> _spoilerLevelCollection = new ObservableCollection<string>();
+        public ObservableCollection<string> SpoilerLevelCollection
         {
-            get
-            {
-                if (_spoilerLevelList == null)
-                {
-                    _spoilerLevelList = new List<KeyValuePair<string, SpoilerLevel>>();
-                    foreach (SpoilerLevel level in Enum.GetValues(typeof(SpoilerLevel)))
-                    {
-                        FieldInfo fieldInfo = level.GetType().GetField(level.ToString());
-                        DescriptionAttribute[] attributes =
-                            (DescriptionAttribute[])fieldInfo.GetCustomAttributes(typeof(DescriptionAttribute), false);
-                        string description = attributes.Length > 0 ? attributes[0].Description : string.Empty;
-                        KeyValuePair<string, SpoilerLevel> typeKeyValue =
-                            new KeyValuePair<string, SpoilerLevel>(description, level);
-                        _spoilerLevelList.Add(typeKeyValue);
-                    }
-                    return _spoilerLevelList;
-                }
-                return _spoilerLevelList;
-            }
+            get { return _spoilerLevelCollection; }
             set
             {
-                _spoilerLevelList = value;
-                RaisePropertyChanged(nameof(SpoilerLevelList));
+                _spoilerLevelCollection = value;
+                RaisePropertyChanged(nameof(SpoilerLevelCollection));
             }
         }
         #endregion
 
         #region SelectedNsfwEnabled
-        private NsfwEnabled _selectedNsfwEnabled;
-        public NsfwEnabled SelectedNsfwEnabled
+        private bool _selectedNsfwEnabled;
+        public bool SelectedNsfwEnabled
         {
             get { return _selectedNsfwEnabled; }
             set
             {
                 _selectedNsfwEnabled = value;
-                switch (value)
-                {
-                    case NsfwEnabled.Yes:
-                        IsNsfwEnabled = true;
-                        break;
-                    case NsfwEnabled.No:
-                        IsNsfwEnabled = false;
-                        break;
-                    default:
-                        IsNsfwEnabled = false;
-                        break;
-                }
                 RaisePropertyChanged(nameof(SelectedNsfwEnabled));
             }
         }
         #endregion
 
-        #region NsfwEnabledList
-        private List<KeyValuePair<string, NsfwEnabled>> _nsfwEnabledList;
-        public List<KeyValuePair<string, NsfwEnabled>> NsfwEnabledList
+        #region NsfwEnabledCollection
+        private ObservableCollection<bool> _nsfwEnabledCollection= new ObservableCollection<bool>();
+        public ObservableCollection<bool> NsfwEnabledCollection
         {
-            get
-            {
-                if (_nsfwEnabledList == null)
-                {
-                    _nsfwEnabledList = new List<KeyValuePair<string, NsfwEnabled>>();
-                    foreach (NsfwEnabled level in Enum.GetValues(typeof(NsfwEnabled)))
-                    {
-                        FieldInfo fieldInfo = level.GetType().GetField(level.ToString());
-                        DescriptionAttribute[] attributes = (DescriptionAttribute[])fieldInfo.GetCustomAttributes(typeof(DescriptionAttribute), false);
-                        string description = attributes.Length > 0 ? attributes[0].Description : string.Empty;
-                        KeyValuePair<string, NsfwEnabled> typeKeyValue = new KeyValuePair<string, NsfwEnabled>(description, level);
-                        _nsfwEnabledList.Add(typeKeyValue);
-                    }
-                    return _nsfwEnabledList;
-                }
-                return _nsfwEnabledList;
-            }
+            get { return _nsfwEnabledCollection; }
             set
             {
-                _nsfwEnabledList = value;
-                RaisePropertyChanged(nameof(NsfwEnabledList));
+                _nsfwEnabledCollection = value;
+                RaisePropertyChanged(nameof(NsfwEnabledCollection));
             }
         }
+
         #endregion
 
-        public bool IsNsfwEnabled { get; set; }
-        public int ChosenSpoilerLevel { get; set; }
+        public int SpoilerLevel { get; set; }
         #endregion
 
         public UserSettingsViewModel()
         {
+            NsfwEnabledCollection.Add(false);
+            NsfwEnabledCollection.Add(true);
             
+            SpoilerLevelCollection.Add("None");
+            SpoilerLevelCollection.Add("Minor");
+            SpoilerLevelCollection.Add("Major");
         }
         
         private void SaveSettings()
         {
-            Globals.NsfwEnabled = IsNsfwEnabled;
-            Globals.MaxSpoiler = ChosenSpoilerLevel;
+            Globals.NsfwEnabled = SelectedNsfwEnabled;
+            Globals.MaxSpoiler = SpoilerLevel;
 
             UserSettings userSettings = new UserSettings
             {
-                NsfwEnabled = IsNsfwEnabled,
-                MaxSpoilerLevel = ChosenSpoilerLevel
+                NsfwEnabled = SelectedNsfwEnabled,
+                MaxSpoilerLevel = SpoilerLevel
             };
             ModifyUserSettings.SaveUserSettings(userSettings);
         }
 
     }
-
-
-    public enum SpoilerLevel
-    {
-        [Description("None")]
-        None = 0,
-        [Description("Minor")]
-        Minor = 1,
-        [Description("Major")]
-        Major = 2
-    }
-
-    public enum NsfwEnabled
-    {
-        [Description("No")]
-        No = 0,
-        [Description("Yes")]
-        Yes = 1
-    }
-
 }
