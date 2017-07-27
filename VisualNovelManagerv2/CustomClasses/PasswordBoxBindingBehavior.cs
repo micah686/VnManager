53a1a1acd6f3ac0cc81e8171df7ed71dc0b4487f
Micah686 <legendofzeldafan3@gmail.com>
2017-07-26 19:59:10 -0700

    Added some basic binding for passwordBox(SecureString ONLY)

---
+++ VisualNovelManagerv2/CustomClasses/PasswordBoxBindingBehavior.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Interactivity;
+
+namespace VisualNovelManagerv2.CustomClasses
+{
+    public class PasswordBoxBindingBehavior: Behavior<PasswordBox>
+    {
+        protected override void OnAttached()
+        {
+            AssociatedObject.PasswordChanged += OnPasswordBoxValueChanged;
+        }
+
+        public SecureString Password
+        {
+            get => (SecureString)GetValue(PasswordProperty);
+            set => SetValue(PasswordProperty, value);
+        }
+
+        public static readonly DependencyProperty PasswordProperty =
+            DependencyProperty.Register("Password", typeof(SecureString), typeof(PasswordBoxBindingBehavior), new PropertyMetadata(null));
+
+
+        private void OnPasswordBoxValueChanged(object sender, RoutedEventArgs e)
+        {
+            BindingExpression binding = BindingOperations.GetBindingExpression(this, PasswordProperty);
+            if (binding == null) return;
+            PropertyInfo property = binding.DataItem.GetType().GetProperty(binding.ParentBinding.Path.Path);
+            if (property != null)
+                property.SetValue(binding.DataItem, AssociatedObject.SecurePassword, null);
+        }
+    }
+}
--- VisualNovelManagerv2/ViewModel/VisualNovels/VnListViewModel.cs
+++ VisualNovelManagerv2/ViewModel/VisualNovels/VnListViewModel.cs
@@ -1,13 +1,70 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
+using System.Security;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using FirstFloor.ModernUI.Presentation;
 using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.CommandWpf;
 
 namespace VisualNovelManagerv2.ViewModel.VisualNovels
 {
     public class VnListViewModel: ViewModelBase
     {
+        #region Properties
+
+        #region VotelistCollection
+        private ObservableCollection<string> _votelistCollection;
+        public ObservableCollection<string> VotelistCollection
+        {
+            get { return _votelistCollection; }
+            set
+            {
+                _votelistCollection = value;
+                RaisePropertyChanged(nameof(VotelistCollection));
+            }
+        }        
+        #endregion
+
+        #region Username
+        private string _username;
+        public string Username
+        {
+            get { return _username; }
+            set
+            {
+                _username = value;
+                RaisePropertyChanged(nameof(Username));
+            }
+        }
+        #endregion
+
+        #region Password
+        private SecureString _password;
+        public SecureString Password
+        {
+            get { return _password; }
+            set
+            {
+                _password = value;
+                RaisePropertyChanged(nameof(Password));
+            }
+        }
+        #endregion
+
+
+
+        #endregion
+
+        public ICommand LoginCommand => new GalaSoft.MvvmLight.Command.RelayCommand(Login);
+        public VnListViewModel() { }
+
+        private void Login()
+        {
+            
+        }
     }
 }
