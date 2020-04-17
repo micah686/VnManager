143489e4078059e3b0fe49ee6ef92cfdb8a3bc4c
Micah K <legendofzeldafan3@gmail.com>
2020-04-16 21:17:57 -0700

    Added skeleton AddGame Window

--- src/VnManager/VnManager/ViewModels/RootViewModel.cs
+++ src/VnManager/VnManager/ViewModels/RootViewModel.cs
@@ -2,10 +2,23 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using VnManager.ViewModels.Windows;
 
 namespace VnManager.ViewModels
 {
     public class RootViewModel: Screen
     {
+        private readonly IWindowManager _windowManager;
+
+        public RootViewModel(IWindowManager windowManager)
+        {
+            _windowManager = windowManager;
+        }
+
+        public void Click()
+        {
+            var vm = new AddGameViewModel(_windowManager);
+            _windowManager.ShowWindow(vm);
+        }
     }
 }
---
+++ src/VnManager/VnManager/ViewModels/Windows/AddGameViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Stylet;
+
+namespace VnManager.ViewModels.Windows
+{
+    public class AddGameViewModel: Screen
+    {
+        private readonly IWindowManager _windowManager;
+
+        public AddGameViewModel(IWindowManager windowManager)
+        {
+            _windowManager = windowManager;
+        }
+    }
+}
