29e736063b539951a66c0b8b42491047ff5c3924
Micah K <legendofzeldafan3@gmail.com>
2020-05-04 21:56:11 -0700

    Added Vndb project and basic RequestOptions

---
+++ src/VnManager/Helpers/Vndb/RequestOptions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using VndbSharp.Interfaces;
+
+namespace VnManager.Helpers.Vndb
+{
+    public class RequestOptions : IRequestOptions
+    {
+        public int? Page { get; set; }
+        public int? Count { get; set; }
+        public string Sort { get; set; }
+        public bool? Reverse { get; set; }
+    }
+}
