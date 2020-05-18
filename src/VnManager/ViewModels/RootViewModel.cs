0a81b08d915118c7887b4b19ad80ac0c61391730
Micah K <legendofzeldafan3@gmail.com>
2020-05-17 17:50:54 -0700

    Added Debug Window toggle for me

--- src/VnManager/ViewModels/RootViewModel.cs
+++ src/VnManager/ViewModels/RootViewModel.cs
@@ -18,23 +18,33 @@ namespace VnManager.ViewModels
 
         private readonly IContainer _container;
         private readonly IWindowManager _windowManager;
+        private int _windowButtonPressedCounter = 0;
 
         public static RootViewModel Instance { get; private set; }
 
+
+        #region SettingsPressed
         private bool _isSettingsPressed;
         public bool IsSettingsPressed
         {
             get => _isSettingsPressed;
             set
             {
-                SetAndNotify(ref _isSettingsPressed, value);
-                if(_isSettingsPressed == true)
+                if(_windowButtonPressedCounter != 0 && value == true)
                 {
+                    SetAndNotify(ref _isSettingsPressed, false);
+                    return;
+                }
+                SetAndNotify(ref _isSettingsPressed, value);
+                if (_isSettingsPressed == true)
+                {
+                    _windowButtonPressedCounter += 1;
                     SettingsIconColor = System.Windows.Media.Brushes.LimeGreen;
                     ActivateSettingsClick();
                 }
                 else
                 {
+                    _windowButtonPressedCounter -= 1;
                     var brush = new SolidColorBrush((System.Windows.Media.Color)Application.Current.TryFindResource(AdonisUI.Colors.ForegroundColor));
                     SettingsIconColor = brush != null ? brush : System.Windows.Media.Brushes.LightSteelBlue;
                     ActivateMainClick();
@@ -43,6 +53,36 @@ namespace VnManager.ViewModels
         }
 
         public System.Windows.Media.Brush SettingsIconColor { get; set; }
+        #endregion
+
+        #region DebugPressed
+        private bool _debugPressed;
+        public bool DebugPressed
+        {
+            get => _debugPressed;
+            set
+            {
+                if(_windowButtonPressedCounter != 0 && value == true)
+                {
+                    SetAndNotify(ref _debugPressed, false);
+                    return;
+                }
+                SetAndNotify(ref _debugPressed, value);
+                if(_debugPressed == true)
+                {
+                    _windowButtonPressedCounter += 1;
+                    DebugClick();
+                }
+                else
+                {
+                    _windowButtonPressedCounter -= 1;
+                    ActivateMainClick();
+                }
+            }
+        }
+
+        #endregion
+
 
         public RootViewModel(IContainer container, IWindowManager windowManager)
         {
@@ -70,6 +110,13 @@ namespace VnManager.ViewModels
         public void ActivateMainClick()
         {
             var vm = _container.Get<MainGridViewModel>();
+            ActivateItem(vm);
+        }
+
+
+        public void DebugClick()
+        {
+            var vm = _container.Get<DebugViewModel>();
             ActivateItem(vm);
         }
 
