8ab16b528026901d07132da9d0da35cb6a642062
Micah K <legendofzeldafan3@gmail.com>
2020-06-04 22:24:59 -0700

    Bound items for Statusbar

--- src/VnManager/ViewModels/UserControls/StatusBarViewModel.cs
+++ src/VnManager/ViewModels/UserControls/StatusBarViewModel.cs
@@ -1,12 +1,50 @@
 ﻿using Stylet;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Text;
+using System.Windows.Media;
+using MahApps.Metro.IconPacks;
+using Brush = System.Drawing.Brush;
+using Color = System.Windows.Media.Color;
 
 namespace VnManager.ViewModels.UserControls
 {
     public class StatusBarViewModel: Screen
     {
-        public StatusBarViewModel() { }
+        public string StatusString { get; set; }
+        public bool IsWorking { get; set; }
+        public string GameCount { get; set; }
+        public string InfoText { get; set; }
+        public bool IsProgressBarVisible { get; set; }
+        public double ProgressBarValue { get; set; }
+        public bool IsProgressBarInfinite { get; set; }
+        public bool IsDatabaseProcessing { get; set; }
+        public PackIconMaterialKind StatusIcon { get; set; }
+        //public Brush StatusIconColor { get; set; }
+
+
+        public StatusBarViewModel()
+        {
+            SetInitialValues();
+        }
+
+        private void SetInitialValues()
+        {
+            StatusString = "Ready";
+            IsWorking = false;
+            GameCount = "0 Items";
+            InfoText = "";
+            IsProgressBarVisible = false;
+            ProgressBarValue = 0;
+            IsProgressBarInfinite = false;
+            IsDatabaseProcessing = false;
+            StatusIcon = PackIconMaterialKind.CheckCircleOutline;
+            //StatusIconColor = new SolidBrush(System.Drawing.Color.LimeGreen);
+
+        }
+
+
+        
     }
 }
