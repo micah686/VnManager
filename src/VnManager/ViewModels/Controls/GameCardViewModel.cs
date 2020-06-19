a3e0da1b76aa3cf1905ea03843a8ed4ace84e1bf
Micah K <legendofzeldafan3@gmail.com>
2020-06-18 23:13:22 -0700

    Added WPF card class/Resource Dict. from MaterialDesignWpfXaml library

---
+++ src/VnManager/ViewModels/Controls/GameCardViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Media.Imaging;
+using Stylet;
+
+namespace VnManager.ViewModels.Controls
+{
+    public class GameCardViewModel: Screen
+    {
+        public BitmapImage CoverImage { get; set; }
+        public string LastPlayedString { get; set; }
+        public string TotalTimeString { get; set; }
+    }
+}
